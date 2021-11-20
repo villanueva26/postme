b315cdd97c5a9b4d60c3b395fccf8ef7eb404199
Adriana <20183L101020@utcv.edu.mx>
2021-11-20 09:48:02 -0600

    Actualizacion final de repositorio

--- src/js/app.js
+++ src/js/app.js
@@ -1,61 +1,62 @@
-//Declaracion de variables globales
-let MAIN; //Ayudarnos a controlar lo que se encuentra dentro del Id="main"
-let MODAL_POST; //Apoyarnos con la parte donde se trabaja el posteo (modal post)
-let BTN_SHOW_POST; //Va a ayudar a controlar internamente los botones, botón de más
-let BTN_CANCEL_POST; //Va a ayudar a controlar internamente los botones, botón de cancelar
+/* Declaracion de variables globales */
+let MAIN;
+let MODAL_POST;
+let BTN_SHOW_POST;
+let BTN_CANCEL_POST;
 let deferredPrompt;
 
-//Poner funciones
-
+// Funciones
 const showPostModal = () => {
-    MAIN.style.display = 'none';
-    MODAL_POST.style.display = 'block';
-    setTimeout(() => {
-      MODAL_POST.style.transform = 'translateY(0)'; //Ayuda a hacer un efecto, sale de la posiciój de abajo hacia arriba
-    }, 1);
-};
-  const closePostModal = () => {
-    MAIN.style.display = 'block';
-    MODAL_POST.style.transform = 'translateY(100vh)';
+  MAIN.style.display = 'none';
+  MODAL_POST.style.display = 'block';
+  setTimeout(() => {
+    MODAL_POST.style.transform = 'translateY(0)';
+  }, 1);
 };
 
-  //Anular el efecto del mensaje de instalación de la app
-  window.addEventListener('beforeinstallprompt', (e) => {
-    console.log('anulando mensaje');
+const closePostModal = () => {
+  MAIN.style.display = 'block';
+  MODAL_POST.style.transform = 'translateY(100vh)';
+};
+
+window.addEventListener('beforeinstallprompt', (e) => {
   e.preventDefault();
   deferredPrompt = e;
 });
 
 // Cuando se cargue todo nuestro DOM
-window.addEventListener('load',async() => {
-    MAIN = document.querySelector('#main');
-    MODAL_POST = document.querySelector('#modal-post-section');
-    BTN_SHOW_POST = document.querySelector('#btn-upload-post');
-    BTN_SHOW_POST.addEventListener('click', showPostModal);
-    BTN_CANCEL_POST = document.querySelector('#btn-post-cancel');
-    BTN_CANCEL_POST.addEventListener('click', closePostModal)
+window.addEventListener('load', async() => {
+  MAIN = document.querySelector('#main');
+  MODAL_POST = document.querySelector('#modal-post-section');
+  BTN_SHOW_POST = document.querySelector('#btn-upload-post');
+  BTN_SHOW_POST.addEventListener('click', showPostModal);
+  BTN_CANCEL_POST = document.querySelector('#btn-post-cancel');
+  BTN_CANCEL_POST.addEventListener('click', closePostModal)
 
-    //Mandar a llamar al service worker
-if('serviceWorker' in navigator)
- {
-    const response=await navigator.serviceWorker.register('sw.js');
-    if(response)
-    {
-      console.log('Service worker registrado');
-    }
- }
+await Notification.requestPermission();
 
- //añadir el evento a la instalación
+if('serviceWorker' in navigator){
+
+const response=await navigator.serviceWorker.register('sw.js');
+
+if (response){
+  const ready=await navigator.serviceWorker.ready;
+  ready.showNotification('Hola usuario',{
+    body:'Este mensaje sonará',
+    vibrate:[200,100,200,100,200,100,200]
+  })
+ 
+}
+}
 
 const bannerInstall = document.querySelector('#banner-install');
-bannerInstall.addEventListener('click', async () => {
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
-    const response = await deferredPrompt.userChoice;
-    if (response.outcome === 'dismissed') {
-      console.error('El usuario canceló la instalación');
-    }
-  }
-});
-
+    bannerInstall.addEventListener('click', async () => {
+      if (deferredPrompt) {
+        deferredPrompt.prompt();
+        const response = await deferredPrompt.userChoice;
+        if (response.outcome === 'dismissed') {
+          console.error('El usuario canceló la instalación');
+        }
+      }
+    });
 });
--- sw.js
+++ sw.js
@@ -1,13 +1,67 @@
-self.addEventListener('install', (event) =>{
-    console.log('Service worker instalado');
-});
+const CACHE_NAME_CORE='CACHE-V1';
+const CACHE_FILES_CORE=[
+    "src/css/app.css",
+    "src/js/app.js",
+    "src/images/icons/icon-144x144.png",
+    "src/images/computer.jpg",
+    "index.html",
+    "/"
+];
+const CACHE_NAME_DYNAMIC='Dynamic-v1';
+
+const CACHE_NAME_INMUTABLE='inmutable-v1';
+const CACHE_FILES_INMUTABLE=[
+    "https://fonts.googleapis.com/icon?family=Material+Icons",
+    "https://code.getmdl.io/1.3.0/material.blue-pink.min.css",
+    "https://code.getmdl.io/1.3.0/material.min.js",
+    "https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Mu4mxK.woff2",
+    "https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2",
+    "https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+    "https://unpkg.com/pwacompat"
+];
+
+self.addEventListener('install', (event)=>{
+    const guardandoCache=caches.open(CACHE_NAME_CORE)
+        .then (cache =>cache.addAll(CACHE_FILES_CORE))
+        .catch(err=>console.error(err.message));
+    const guardandoCacheInmutable=caches.open(CACHE_NAME_INMUTABLE)
+        .then (cache =>cache.addAll(CACHE_FILES_INMUTABLE))
+        .catch(err=>console.error(err.message));   
+    self.skipWaiting();
+    event.waitUntil(Promise.all([guardandoCache,guardandoCacheInmutable]));
+    });
 
 self.addEventListener('activate', async (event) =>{
     console.log('sw: Activo y listo para controlar');
+    const obtenerCaches = caches.keys()
+    .then(allCaches => allCaches.filter(cache => ![CACHE_NAME_CORE, CACHE_NAME_INMUTABLE, CACHE_NAME_DYNAMIC].includes(cache)).filter(cacheName => caches.delete(cacheName))) 
+    .catch(err => console.error(err.message))
+  console.info('[SW]: Cache limpiado exitosamente...');
+  event.waitUntil(obtenerCaches);
 });
 
-self.addEventListener('fetch', (event) =>{
-    console.log('Service worker escuchando');
+self.addEventListener('fetch',  (event)=>{
+    if(!(event.request.url.indexOf('http') === 0)){
+        return;
+      }
+
+      const cacheAyudaRed = caches.match(event.request)
+        .then(page => page || fetch(event.request)
+        .then(eventRequest => {
+          return caches.open(CACHE_NAME_DYNAMIC).then(cache => {
+            if (![].concat(CACHE_FILES_CORE,CACHE_FILES_INMUTABLE).indexOf(event.request.url) || eventRequest.type === 'opaque') {
+              cache.put(event.request, eventRequest.clone())
+              }
+              return eventRequest;
+              })
+            }));
+      event.respondWith(cacheAyudaRed);    
 });
 
+self.addEventListener('sync',  (event)=>{
+    console.log(event);
+});
 
+self.addEventListener('push',  (event)=>{
+    console.error(event);
+});
